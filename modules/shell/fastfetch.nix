41bbed83d15fc73be2c85febd3663a4014d2cea3
Theo Park <theo.park.n@gmail.com>
2026-08-18 17:21:08 -0400

    feat(modules): migrate shell/terminal aspects
    fastfetch, fish, starship, zsh

---
+++ modules/shell/fastfetch.nix
@@ -0,0 +1,36 @@
+{
+  flake.modules.homeManager.fastfetch = {
+
+    programs.fastfetch = {
+      enable = true;
+      settings = {
+        logo = {
+          type= "small";
+        };
+        modules = [
+          "separator"
+          {
+            "type" = "title";
+            "format" = "{host-name-colored}";
+          }
+          "separator"
+
+          "os"
+          "packages"
+          "wm"
+
+          "host"
+
+          "terminal"
+          "shell"
+          "terminalfont"
+
+          "break"
+          "colors"
+          "break"
+        ];
+      };
+    };
+
+  };
+}
