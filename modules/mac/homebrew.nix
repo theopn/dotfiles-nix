8116b5a3bead4e12e105695bf2e13a88b4077b65
Theo Park <theo.park.n@gmail.com>
2026-08-18 17:22:04 -0400

    feat(modules): migrate macos aspects (aerospace, homebrew)

---
+++ modules/mac/homebrew.nix
@@ -0,0 +1,81 @@
+{
+  flake.modules.darwin.homebrew = { ... }:
+    let
+      externalAppDir = "/Volumes/theo-crucial-p310/Applications";
+    in
+    {
+      homebrew = {
+        enable = true;
+        casks = [
+          # Dev
+          "docker-desktop"
+          #{
+          # name = "docker-desktop";
+          # args = { appdir = externalAppDir };
+          #}
+          #"kicad"
+
+          # Fun & Media
+          "gimp"
+          #{
+          # name = "gimp";
+          # args = { appdir = externalAppDir };
+          #}
+          #"minecraft"
+          "obs"
+          #{
+          # name = "obs";
+          # args = { appdir = externalAppDir };
+          #}
+          "spotify"
+
+          # Productivity
+          "itsycal"
+          "notion"
+          "zotero"
+          #{
+          # name = "zotero";
+          # args = { appdir = externalAppDir };
+          #}
+
+          # Sync
+          "filen"
+          #{
+          # name = "filen";
+          # args = { appdir = externalAppDir };
+          #}
+          "syncthing-app"
+
+          # System
+          "jordanbaird-ice"
+          "stats"
+
+          # Tools
+          "keycastr"
+          #{
+          # name = "keycastr";
+          # args = { appdir = externalAppDir };
+          #}
+          "skim"
+
+          # Web
+          "brave-browser"
+          #{
+          # name = "brave-browser";
+          # args = { appdir = externalAppDir };
+          #}
+          "discord"
+          "firefox"
+          "tailscale-app"
+          "thunderbird"
+        ];
+
+        # Delete unspecified Homebrew formulae
+        onActivation = {
+          autoUpdate = true;
+          upgrade = true;
+          cleanup = "zap";
+        };
+      };
+    };
+}
