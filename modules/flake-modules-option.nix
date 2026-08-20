53d2cd63b147834f7758b2f5752b604093e3402b
Theo Park <theo.park.n@gmail.com>
2026-08-19 22:42:15 -0400

    style: improve the flake.modules option description

--- modules/flake-modules-option.nix
+++ modules/flake-modules-option.nix
@@ -2,12 +2,15 @@
 {
   options.flake = flake-parts-lib.mkSubmoduleOptions {
     modules = lib.mkOption {
+
       type = lib.types.lazyAttrsOf (lib.types.lazyAttrsOf lib.types.deferredModule);
+
       default = { };
+
       description = ''
-        Dendritic-pattern aspect registry: flake.modules.<class>.<name>.
-        Each <class> (nixos, homeManager, darwin, ...) holds named,
-        composable modules that hosts import by name.
+        Theo's custom Dendritic pattern registry: flake.modules.<class>.<name>.
+        Each <class> (nixos, homeManager, darwin, ...) holds named modules
+        that hosts can import.
       '';
     };
   };
