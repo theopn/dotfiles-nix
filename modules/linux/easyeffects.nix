68235957d8b9797ef435eee61202cc467f89dd91
Theo Park <theo.park.n@gmail.com>
2026-08-21 12:09:41 -0400

    chore: remove unused IRS & FW13 preset from easyeffects config

--- modules/linux/easyeffects.nix
+++ modules/linux/easyeffects.nix
@@ -1,18 +1,6 @@
 {
   flake.modules.homeManager.easyeffects = { pkgs, ... }:
   let
-    irs = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/FrameworkComputer/linux-docs/main/easy-effects/irs/IR_22ms_27dB_5t_15s_0c.irs";
-      # https://github.com/FrameworkComputer/linux-docs/commit/f2d52e46b456b34717fc574bf18987ea525c59e3
-      hash = "sha256-IxDXNhnTg/NrhPxA5+6u/meEnlX720eoQPyoJfbuge0=";
-    };
-
-    # fw13-preset = pkgs.fetchurl {
-    #   url = "https://raw.githubusercontent.com/FrameworkComputer/linux-docs/main/easy-effects/fw13-easy-effects.json";
-    #   # https://github.com/FrameworkComputer/linux-docs/commit/e5289ecc283e0e940536ce48e0ed789adf0280be
-    #   hash = "sha256-SNSUu858z6+RdcRH9xgBTJeSDJmLu2RINx+OUaWGH3A=";
-    # };
-
     fw16-preset = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/FrameworkComputer/linux-docs/main/easy-effects/fw16-easy-effects.json";
       # https://github.com/FrameworkComputer/linux-docs/commit/46ca475967323505d700760f78c7a3202840ef61
@@ -21,8 +9,6 @@
   in
   {
     xdg.configFile = {
-      "easyeffects/irs/IR_22ms_27dB_5t_15s_0c.irs".source = irs;
-      #"easyeffects/output/fw13-easy-effects.json".source = fw13-preset;
       "easyeffects/output/fw16-easy-effects.json".source = fw16-preset;
     };
 
