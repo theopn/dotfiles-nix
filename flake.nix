fe270675fe2a5b273eb0e52b16bef9b8f9b0a9b5
Theo Park <theo.park.n@gmail.com>
2026-08-20 21:06:38 -0400

    fix: apparently Discord doesn't respect the global mime, only the user specified ones. Migrated to home-manager mime module

--- flake.nix
+++ flake.nix
@@ -112,7 +112,7 @@
                 syncthing
 
                 # modules/linux
-                easyeffects keychain nm-applet mate-polkit udiskie
+                easyeffects keychain nm-applet mate-polkit udiskie xdg-mime
                 # modules/wm
                 cliphist dconf dunst gammastep niri rofi swayidle swaylock theme waybar
                 # modules/media
--- modules/base/nixos.nix
+++ modules/base/nixos.nix
@@ -44,28 +44,5 @@
       LESSHISTFILE = "-";
     };
 
-
-    # default applications
-    # ls -l /run/current-system/sw/share/applications/ /etc/profiles/per-user/${USER}/share/applications/
-    xdg.mime.defaultApplications = {
-      "text/html"                = [ "librewolf.desktop" "firefox.desktop" ];
-      "application/xhtml+xml"    = [ "librewolf.desktop" "firefox.desktop" ];
-      "x-scheme-handler/http"    = [ "librewolf.desktop" "firefox.desktop" ];
-      "x-scheme-handler/https"   = [ "librewolf.desktop" "firefox.desktop" ];
-      "x-scheme-handler/about"   = [ "librewolf.desktop" "firefox.desktop" ];
-      "x-scheme-handler/unknown" = [ "librewolf.desktop" "firefox.desktop" ];
-      "x-scheme-handler/mailto"  = [ "librewolf.desktop" "firefox.desktop" ];
-
-      "image/*" = [ "imv-dir.desktop" "gimp.desktop" ];
-      "video/*" = "mpv.desktop";
-      "audio/*" = "mpv.desktop";
-
-      "application/pdf"      = "org.pwmt.zathura.desktop";
-      "application/epub+zip" = "org.pwmt.zathura.desktop";
-
-      "text/*" = "neovide.desktop";
-      "inode/directory" = "lf.desktop";
-    };
-
   };
 }
